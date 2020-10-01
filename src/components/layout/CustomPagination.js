ba21414f4269199182e368abe38649c9b2ccddc5
Deap Daru <deapdaru@gmail.com>
2020-10-01 14:26:01 +0530

    responsive bug fix for pagination

--- src/components/layout/CustomPagination.js
+++ src/components/layout/CustomPagination.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Pagination from '@material-ui/lab/Pagination';
-import { Grid } from '@material-ui/core';
+import { Grid, Hidden } from '@material-ui/core';
 
 function CustomPagination(props) {
     const handleEvent = (event, value) => {
@@ -9,7 +9,12 @@ function CustomPagination(props) {
 
     return (
         <Grid container justify="space-around">
-            <Pagination count={10} page={props.page + 1} onChange={handleEvent} variant="outlined" color="secondary" showFirstButton showLastButton boundaryCount={3} />
+            <Hidden xsDown>
+                <Pagination count={10} page={props.page + 1} onChange={handleEvent} variant="outlined" color="secondary" showFirstButton showLastButton boundaryCount={3} />
+            </Hidden>
+            <Hidden smUp>
+                <Pagination size="small" count={10} page={props.page + 1} onChange={handleEvent} variant="outlined" color="secondary" boundaryCount={2} />
+            </Hidden>
         </Grid>
     )
 }
